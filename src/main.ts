2223c9fc73ed7f9aaedbab34ffd823dc6916856e
Alfredo Barragan <64043214+PastaChicken@users.noreply.github.com>
2025-10-10 02:03:19 +0000

    S2 Submission
    Co-authored-by: BeReyes1 <BeReyes1@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,13 @@ import "./style.css";
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
+
+document.body.innerHTML = ` 
+  <p> I make change </p>
+
+`;
+
+//make a button first
+//<button class="favorite styled" type="button">Add to favorites</button>
+
+console.log("something");
