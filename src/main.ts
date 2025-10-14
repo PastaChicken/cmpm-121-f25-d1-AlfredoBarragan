cc13a842533ebbe35c93ff6c03edb13865b927a7
Alfredo Barragan <64043214+PastaChicken@users.noreply.github.com>
2025-10-14 21:10:08 +0000

    Step 4 complete: added Continuous growth with requestAnimationFrame

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,40 @@
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-function startCounterInterval(callback: () => void, interval: number) {
-  return setInterval(callback, interval);
-}
-
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
 
 const counterDiv = document.createElement("div");
 let counter = 0;
-
+let cumulativecounter = 0;
+//Initial text
 counterDiv.textContent = `${counter} Pizza's`;
 
 document.body.appendChild(counterDiv);
 
+//Create a button
 const button = document.createElement("button");
 button.textContent = "🍕";
 document.body.appendChild(button);
 
+//Add event listener to the button
 button.addEventListener("click", () => {
   counter++;
-  counterDiv.textContent = `${counter} Pizza's`;
+  counterDiv.textContent = `${counter + cumulativecounter} Pizza's`;
 });
 
-startCounterInterval(() => {
-  counter++;
-  counterDiv.textContent = `${counter} Pizza's`;
-}, 1000);
+let lastTime = performance.now();
+
+function updateCounter(now: number) {
+  if (now - lastTime >= 1000) {
+    cumulativecounter += 1;
+    counter++;
+    counter = counter + cumulativecounter;
+    counterDiv.textContent = `${counter} Pizza's`;
+
+    lastTime = now;
+  }
+  requestAnimationFrame(updateCounter);
+}
+requestAnimationFrame(updateCounter);
