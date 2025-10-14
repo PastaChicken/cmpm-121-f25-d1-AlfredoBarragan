cb022e07cf4350bc316410133ca64da6c48d5b9f
Alfredo Barragan <64043214+PastaChicken@users.noreply.github.com>
2025-10-14 21:50:35 +0000

    Step 5 complete added an upgrade button to buy auto upgrades.

--- src/main.ts
+++ src/main.ts
@@ -6,35 +6,64 @@ document.body.innerHTML = `
 `;
 
 const counterDiv = document.createElement("div");
+const currentAutoUpgrades = document.createElement("div");
 let counter = 0;
 let cumulativecounter = 0;
-//Initial text
 counterDiv.textContent = `${counter} Pizza's`;
 
 document.body.appendChild(counterDiv);
 
-//Create a button
+//Create click button
 const button = document.createElement("button");
 button.textContent = "🍕";
+button.style.fontSize = "2rem";
+button.style.margin = "0.5rem";
 document.body.appendChild(button);
 
-//Add event listener to the button
+//Upgrade button
+const upgrade = document.createElement("button");
+upgrade.textContent = "Upgrade: Auto Pizza";
+const upgradeText = document.createElement("span");
+upgradeText.textContent = " (Cost: 10 Pizza's)";
+
+//display texts
+document.body.appendChild(upgrade);
+document.body.appendChild(upgradeText);
+document.body.appendChild(currentAutoUpgrades);
+
+//add event listener to the button
 button.addEventListener("click", () => {
   counter++;
-  counterDiv.textContent = `${counter + cumulativecounter} Pizza's`;
+  counterDiv.textContent = `${counter} Pizza's`;
+});
+
+//add event listener to the upgrade button
+upgrade.addEventListener("click", () => {
+  if (counter >= 10) {
+    counter = counter - 10;
+    upgrade.textContent = "Auto Pizza Upgrade Purchased!";
+    counterDiv.textContent = `${counter} Pizza's`;
+    cumulativecounter += 1;
+    currentAutoUpgrades.textContent =
+      ` Current Auto clicks per Second: ${cumulativecounter}`;
+
+    requestAnimationFrame(updateCounter);
+  } else {
+    upgrade.textContent = "Not enough Pizza's. required 10 Pizza's";
+  }
 });
 
 let lastTime = performance.now();
 
 function updateCounter(now: number) {
   if (now - lastTime >= 1000) {
-    cumulativecounter += 1;
-    counter++;
     counter = counter + cumulativecounter;
     counterDiv.textContent = `${counter} Pizza's`;
 
     lastTime = now;
   }
+  if (counter >= 10) {
+    upgrade.textContent = "Upgrade: Auto Pizza (Available!)";
+  }
   requestAnimationFrame(updateCounter);
 }
-requestAnimationFrame(updateCounter);
