61c31b3b257cc5df8478b4c3c8290e3bf2a2d00e
Alfredo Barragan <64043214+PastaChicken@users.noreply.github.com>
2025-10-14 20:12:30 +0000

    added button click logic

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ document.body.innerHTML = `
 
 `;
 
-const button = document.createElement('button');
-button.textContent = 'Click me';
+const button = document.createElement("button");
+button.textContent = "Click me";
 document.body.appendChild(button);
